ae0ac5016988b0d7d4b54f554c0459da38058b4f
haech06 <akaruniaputri@gmail.com>
2024-06-09 10:23:25 +0700

    cb

--- dbdbdb.php
+++ dbdbdb.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost"; // Lokasi server MySQL
-$username = "root"; // Nama pengguna MySQL
-$password = ""; // Kata sandi MySQL
-$database = "elevent"; // Nama database MySQL
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "elevent"; 
 
-// Membuat koneksi
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Memeriksa koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 echo "Koneksi berhasil";
+
+echo "halo"
 ?>
